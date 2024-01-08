ce4953fa282c761d9e74b9a5894acf4f5492a29a
ShokhAke <webcoder014@gmail.com>
2024-01-08 15:06:36 +0500

    Js da yangi o'zgarishlar

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const seriesDB = {
     privat: false
 };
 
+// 3
 let a = prompt ("Oxirgi ko'rgan serialingiz?", ""),
     b = prompt ("Nechi baho berasiz", ""),
     c = prompt ("Oxirgi ko'rgan serialingiz?", ""),
@@ -20,3 +21,23 @@ seriesDB.series [a] = b;
 seriesDB.series [c] = d;
 
 console.log(seriesDB);
+
+// const singer = prompt ("Qaysi qo'shiqchini eshitasiz", "");
+// console.log(singer);
+
+// const seriesDB = {
+//         count: singer,
+//         series: {},
+//         actors: {},
+//         genres: [],
+//         privat: false
+//     };
+
+// let a = prompt ("Qaysi treki yoqadi", ""),  
+//     b = prompt ("Nechi baho berasiz", ""),  
+//     c = prompt ("Qaysi treki yoqadi", ""),
+//     d = prompt ("Nechi baho berasiz", "");
+
+// seriesDB.series [a] = b;
+// seriesDB.series [c] = d; 
+// console.log(seriesDB);
